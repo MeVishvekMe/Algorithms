ae2d5204ae01c35a5423bf9f880f012e97dceaa9
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-08-26 20:03:46 +0530

    updated binarySearch

--- Binary Search/Main.java
+++ Binary Search/Main.java
@@ -1,17 +1,18 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
-        System.out.println(binarySearchIterative(arr, 10));
+        System.out.println(binarySearchIterative(arr, 3));
+        System.out.println(binarySearchRecursive(arr, 3, 0, arr.length - 1));
     }
 
-    public static boolean binarySearchIterative(int[] arr, int target) {
+    public static int binarySearchIterative(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
         int middle = (left + right) / 2;
 
         while(left <= right) {
             if(arr[middle] == target) {
-                return true;
+                return middle;
             }
             else if(target > arr[middle]) {
                 left = middle + 1;
@@ -21,23 +22,22 @@ class Main {
             }
             middle = (left + right) / 2;
         }
-        return false;
+        return -1;
     }
 
-    public static boolean binarySearchRecursive(int[] arr, int target, int left, int right) {
-        if(left > right || right < left) {
-            return false;
-        }
+    public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
         int middle = (left + right) / 2;
+        if(left > right || right < left) {
+            return -1;
+        }
         if(arr[middle] == target) {
-            return true;
+            return middle;
         }
         else if(target > arr[middle]) {
-            binarySearchIterative(arr, target, middle + 1, right);
+            return binarySearchRecursive(arr, target, middle + 1, right);
         }
         else {
-            right = middle - 1;
+            return binarySearchRecursive(arr, target, left, middle - 1);
         }
-        middle = (left + right) / 2;
     }
 }
--- Linear Search/Main.java
+++ Linear Search/Main.java
@@ -1,18 +1,28 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
-        System.out.println(linearSearch(arr, 20));
-        
+        System.out.println(linearSearchIterative(arr, 9));
+        System.out.println(linearSearchRecursive(arr, 9, 0));
     }
 
-    public static boolean linearSearch(int[] arr, int target) {
+    public static int linearSearchIterative(int[] arr, int target) {
         int i = 0;
         while(i < arr.length) {
             if(arr[i] == target) {
-                return true;
+                return i;
             }
             i++;
         }
-        return false;
+        return -1;
+    }
+
+    public static int linearSearchRecursive(int[] arr, int target, int i) {
+        if(i > arr.length || i < 0) {
+            return -1;
+        }
+        else if(arr[i] == target) {
+            return i;
+        }
+        return linearSearchRecursive(arr, target, i + 1);
     }
 }
